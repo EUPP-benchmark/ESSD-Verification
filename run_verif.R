7a31da8215d0148fa1b989af5d8a0da98637b388
aitaten <aitor.atencia@zamg.ac.at>
2022-10-14 18:54:35 +0200

    File to run functions to obtain a matrix of verification

---
+++ run_verif.R
@@ -0,0 +1,8 @@
+source("read_data.R")
+source("verif_functions.R")
+
+#Read all the data (This is also deleting the previous variables)
+all_data <- read_data_from_path()
+
+#Compute several scores as a function of lead-time and Station
+ll_veri <- verif_for_st_lt(all_data)
